e9679764c816baa776ea86363672f1f49b60b50f
Paul <paulyk1983@gmail.com>
2016-06-08 15:03:44 -0500

    started adding code to dashboard controller that gets existing data from csv file and populates the database

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,18 @@
+require 'csv'
+
 class DashboardController < ApplicationController
   def show
+    @file = CSV.read('test.csv')
+    file.each_with_index do |row, i|
+      unless i == 0
+        Inquiry.create(
+          user_id: current_user.id,
+          company: row[0],
+          first_name: row[1],
+          last_name: row[2],
+          email: row[3]
+        )
+      end
+    end
   end
 end
