9647b8701e35198277adff500ecd928264eddeab
Paul <paulyk1983@gmail.com>
2016-06-22 16:31:53 -0500

    added code to test api on heroku server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery :except => [:create]
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+  
+  protected
 
-  skip_before_action :verify_authenticity_token
+  def json_request?
+    request.format.json?
+  end
 end
