b33ebe38b6a69698dc21d4b7a951ba4fc4ce0ded
Paul <paulyk1983@gmail.com>
2016-06-25 14:28:33 -0500

    cleaned up code by moving business logic and send mail command from inquiries controller to model

--- app/controllers/api/v1/inquiries_controller.rb
+++ app/controllers/api/v1/inquiries_controller.rb
@@ -35,32 +35,9 @@ class Api::V1::InquiriesController < ApplicationController
       attachment: params[:attachment]
     )
 
-    body = inquiry.first_name + ' from ' + inquiry.company_name + ' has requested a quote for ' + inquiry.material + ' ' + inquiry.product + ' 
+    inquiry.send_request_alert(inquiry)
 
-    Quantity: ' + inquiry.quantity + ' 
-
-    Lead Time: ' + inquiry.lead_time + ' 
-
-    In Hand Date: ' + inquiry.in_hand_date.to_s + ' 
-
-    See all details: http://localhost:3000/inquiries/' + inquiry.id.to_s + ''
-
-    Mail.defaults do
-      delivery_method :smtp, {
-        :address => 'smtp.gmail.com',
-        :port => '587',
-        :user_name => ENV['EMAIL_USER'],
-        :password => ENV['EMAIL_PASSWORD'],
-        :authentication => :plain
-      }
-    end
-
-    Mail.new(
-      :to => 'paulyk1983@gmail.com',
-      :from => 'paulyk1983@gmail.com',
-      :subject => 'New Quote Request',
-      :body => body
-    ).deliver!
+    
 
     redirect_to 'https://finishlinecorp.com/test'
   end
--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -4,4 +4,33 @@ class Inquiry < ActiveRecord::Base
   belongs_to :company
   has_many :quotes
   has_many :samples
+
+  def send_request_alert(inquiry)
+    body = inquiry.first_name + ' from ' + inquiry.company_name + ' has requested a quote for ' + inquiry.material + ' ' + inquiry.product + ' 
+
+    Quantity: ' + inquiry.quantity + ' 
+
+    Lead Time: ' + inquiry.lead_time + ' 
+
+    In Hand Date: ' + inquiry.in_hand_date.to_s + ' 
+
+    See all details: http://localhost:3000/inquiries/' + inquiry.id.to_s + ''
+
+    Mail.defaults do
+      delivery_method :smtp, {
+        :address => 'smtp.gmail.com',
+        :port => '587',
+        :user_name => ENV['EMAIL_USER'],
+        :password => ENV['EMAIL_PASSWORD'],
+        :authentication => :plain
+      }
+    end
+
+    Mail.new(
+      :to => 'paulyk1983@gmail.com',
+      :from => 'paulyk1983@gmail.com',
+      :subject => 'New Quote Request',
+      :body => body
+    ).deliver!
+  end
 end
