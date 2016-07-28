bb903e6c7be79efd30e55c1e50e504bf9cc0676c
Paul <paulyk1983@gmail.com>
2016-07-27 22:13:18 -0500

    added inquiries edit view and changed inquiries controller show method to change inquiry status to rejected if params  status equals rejected

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,19 +1,24 @@
 class InquiriesController < ApplicationController
   def show
     @inquiry = Inquiry.find_by(id: params[:id])
+    status = params[:status]
+    if status == "reject" 
+      @inquiry.update(status: "rejected")  
+      # insert flash message
+    end
   end
 
   def edit
     @inquiry = Inquiry.find_by(id: params[:id])
   end
 
-  def update
-    @inquiry = Inquiry.find_by(id: params[:id])
-    status = params[:status]
-    if status == "reject" 
-      @inquiry.update(status: "rejected")  
-    end
+  # def update
+  #   @inquiry = Inquiry.find_by(id: params[:id])
+  #   status = params[:status]
+  #   if status == "reject" 
+  #     @inquiry.update(status: "rejected")  
+  #   end
     
-    redirect_to "/inquiries/#{@inquiry.id}"
-  end
+  #   redirect_to "/inquiries/#{@inquiry.id}"
+  # end
 end
