15a28cdd5132b3dc459b854b25a2c8ecd68e7050
Paul <paulyk1983@gmail.com>
2016-07-27 22:00:04 -0500

    added edit and update controllers for inquiries

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -2,4 +2,18 @@ class InquiriesController < ApplicationController
   def show
     @inquiry = Inquiry.find_by(id: params[:id])
   end
+
+  def edit
+    @inquiry = Inquiry.find_by(id: params[:id])
+  end
+
+  def update
+    @inquiry = Inquiry.find_by(id: params[:id])
+    status = params[:status]
+    if status == "reject" 
+      @inquiry.update(status: "rejected")  
+    end
+    
+    redirect_to "/inquiries/#{@inquiry.id}"
+  end
 end
