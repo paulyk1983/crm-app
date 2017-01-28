e08197cd50f329d824fa88044d7ef058a052ec02
Paul <paulyk1983@gmail.com>
2017-01-28 17:21:15 -0600

    added empty contacts angular controller

---
+++ app/assets/javascripts/contacts_ctrl.js
@@ -0,0 +1,9 @@
+/* global angular */
+(function() {
+  angular.module('app').controller("contactsCtrl", ['$scope', '$http', function($scope, $http) {
+   
+    $scope.test = 'Angular controller is working!'
+
+  }]);
+
+})();
