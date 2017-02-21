602cc523626f527cf1779ad1457d7c0965c61722
Paul <paulyk1983@gmail.com>
2017-02-21 14:36:59 -0600

    added dynamic email template

---
+++ app/assets/javascripts/inquiries_ctrl.js
@@ -0,0 +1,24 @@
+/* global angular */
+(function() {
+  angular.module('app').controller("inquiriesCtrl", ['$scope', '$http', function($scope, $http) {
+   
+    $scope.setup = function() {
+      $http.get('/api/v1/inquiries.json').then(function(response) {
+        $scope.inquiries = response.data;
+        $scope.attribute = "first_name";
+        $scope.sortOrder = false;
+      });
+    };
+
+    $scope.changeOrder = function(attribute) {
+      if ($scope.sortOrder) {
+        $scope.sortOrder = false;  
+      } else {
+        $scope.sortOrder = true;
+      }
+      $scope.attribute = attribute;
+    }
+
+  }]);
+
+})();
--- app/views/api/v1/inquiries/index.json.jbuilder
+++ app/views/api/v1/inquiries/index.json.jbuilder
@@ -1,4 +1,9 @@
 json.array! @inquiries.each do |inquiry|
   json.id inquiry.id
+  json.company_id inquiry.company_id
+  json.created_at inquiry.created_at
   json.first_name inquiry.first_name
+  json.last_name inquiry.last_name
+  json.email inquiry.email
+  json.phone_number inquiry.phone_number
 end
